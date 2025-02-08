01f97de53ccc363f30c58a67845637aa9fcdb8b0
Alexandre Oliveira <alexandrebo@gmail.com>
2025-02-08 16:28:07 +0100

    fix 'Warning: [antd: Menu] children is deprecated.'

--- frontend/src/Components/MenuBolao.jsx
+++ frontend/src/Components/MenuBolao.jsx
@@ -1,336 +1,302 @@
 import { Menu } from "antd";
 
-const MenuBolao = (props) => {
-	const { component, setComponent } = props;
+const MenuBolao = ({ component, setComponent }) => {
+	const menuItems = [
+		{
+			key: "s2023",
+			label: "2023",
+			children: [
+				{
+					type: "group",
+					label: "Série A",
+					children: [
+						{
+							key: "21",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "A",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "20",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "A",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "19",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "A",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+				{
+					type: "group",
+					label: "Série B",
+					children: [
+						{
+							key: "18",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "B",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "17",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "B",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "16",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2023,
+									serie: "B",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+			],
+		},
+		{
+			key: "s2022",
+			label: "2022",
+			children: [
+				{
+					type: "group",
+					label: "Série A",
+					children: [
+						{
+							key: "15",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "A",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "14",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "A",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "13",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "A",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+				{
+					type: "group",
+					label: "Série B",
+					children: [
+						{
+							key: "12",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "B",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "11",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "B",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "10",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2022,
+									serie: "B",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+			],
+		},
+		{
+			key: "s2021",
+			label: "2021",
+			children: [
+				{
+					type: "group",
+					label: "Série A",
+					children: [
+						{
+							key: "9",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "A",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "8",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "A",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "7",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "A",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+				{
+					type: "group",
+					label: "Série B",
+					children: [
+						{
+							key: "6",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "B",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "5",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "B",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "4",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2021,
+									serie: "B",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+			],
+		},
+		{
+			key: "s2020",
+			label: "2020",
+			children: [
+				{
+					type: "group",
+					label: "Série A",
+					children: [
+						{
+							key: "3",
+							label: "Classificação",
+							onClick: () =>
+								setComponent({
+									ano: 2020,
+									serie: "A",
+									page: "classificacao",
+								}),
+						},
+						{
+							key: "2",
+							label: "Apostas",
+							onClick: () =>
+								setComponent({
+									ano: 2020,
+									serie: "A",
+									page: "apostas",
+								}),
+						},
+						{
+							key: "1",
+							label: "Distância",
+							onClick: () =>
+								setComponent({
+									ano: 2020,
+									serie: "A",
+									page: "distancia",
+								}),
+						},
+					],
+				},
+			],
+		},
+		// {
+		// 	key: "s8",
+		// 	label: "Ranking",
+		// 	onClick: () => setComponent({ ...component, page: "ranking" }),
+		// },
+		{
+			key: "s7",
+			label: "Regras",
+			onClick: () => setComponent({ ...component, page: "regras" }),
+		},
+	];
+
 	return (
-		<Menu theme="dark" mode="horizontal" defaultSelectedKeys={["s1"]}>
-			{/* <Menu.Item
-				key="22"
-				onClick={() =>
-					setComponent({
-						ano: 2023,
-						serie: "A",
-						page: "addAposta",
-					})
-				}
-			>
-				Adicionar Aposta A
-			</Menu.Item>
-			<Menu.Item
-				key="23"
-				onClick={() =>
-					setComponent({
-						ano: 2023,
-						serie: "B",
-						page: "addAposta",
-					})
-				}
-			>
-				Adicionar Aposta B
-			</Menu.Item> */}
-
-			<Menu.SubMenu key="s1" title="2023">
-				<Menu.ItemGroup title="Série A">
-					<Menu.Item
-						key="1"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "A",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="2"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "A",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="3"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "A",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-				<Menu.ItemGroup title="Série B">
-					<Menu.Item
-						key="4"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "B",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="5"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "B",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="6"
-						onClick={() =>
-							setComponent({
-								ano: 2023,
-								serie: "B",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-			</Menu.SubMenu>
-
-			<Menu.SubMenu key="s2" title="2022">
-				<Menu.ItemGroup title="Série A">
-					<Menu.Item
-						key="7"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "A",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="8"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "A",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="9"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "A",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-				<Menu.ItemGroup title="Série B">
-					<Menu.Item
-						key="10"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "B",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="11"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "B",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="12"
-						onClick={() =>
-							setComponent({
-								ano: 2022,
-								serie: "B",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-			</Menu.SubMenu>
-
-			<Menu.SubMenu key="s3" title="2021">
-				<Menu.ItemGroup title="Série A">
-					<Menu.Item
-						key="13"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "A",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="14"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "A",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="15"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "A",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-				<Menu.ItemGroup title="Série B">
-					<Menu.Item
-						key="16"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "B",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="17"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "B",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="18"
-						onClick={() =>
-							setComponent({
-								ano: 2021,
-								serie: "B",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-			</Menu.SubMenu>
-
-			<Menu.SubMenu key="s4" title="2020">
-				<Menu.ItemGroup title="Série A">
-					<Menu.Item
-						key="19"
-						onClick={() =>
-							setComponent({
-								ano: 2020,
-								serie: "A",
-								page: "classificacao",
-							})
-						}
-					>
-						Classificação
-					</Menu.Item>
-					<Menu.Item
-						key="20"
-						onClick={() =>
-							setComponent({
-								ano: 2020,
-								serie: "A",
-								page: "apostas",
-							})
-						}
-					>
-						Apostas
-					</Menu.Item>
-					<Menu.Item
-						key="21"
-						onClick={() =>
-							setComponent({
-								ano: 2020,
-								serie: "A",
-								page: "distancia",
-							})
-						}
-					>
-						Distância
-					</Menu.Item>
-				</Menu.ItemGroup>
-			</Menu.SubMenu>
-
-			{/* <Menu.Item
-				key="24"
-				onClick={() =>
-					setComponent({
-						...component,
-						page: "ranking",
-					})
-				}
-			>
-				Ranking
-			</Menu.Item> */}
-
-			<Menu.Item
-				key="25"
-				onClick={() =>
-					setComponent({
-						...component,
-						page: "regras",
-					})
-				}
-			>
-				Regras
-			</Menu.Item>
-		</Menu>
+		<Menu
+			theme="dark"
+			mode="horizontal"
+			defaultSelectedKeys={["s1"]}
+			items={menuItems}
+		/>
 	);
 };
 
