6fcf908c36c857732b7d7d51b5b8ceec4fef94c8
Alexandre Oliveira <alexandrebo@gmail.com>
2025-02-09 02:57:19 +0100

    fix 'Warning: useNewUrlParser is a deprecated option'

--- db/index.js
+++ db/index.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
-	.connect(MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+	.connect(MONGO_URI)
 	.then((x) => {
 		console.log(
 			`Connected to Mongo! Database name: "${x.connections[0].name}"`
